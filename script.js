3c2ed741578da2fa3353878c3e39fbabd1728e8f
davizin-ra <daviramos1703@gmail.com>
2025-07-16 04:23:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMenu() {
+  const navLinks = document.getElementById("navLinks");
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".nav-links a");
+  const navLinks = document.getElementById("navLinks");
+
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      if (window.innerWidth <= 768) {
+        navLinks.style.display = "none";
+      }
+    });
+  });
+});
